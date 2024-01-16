19dc90e07814ac50771abcec11a77197acfe5d10
das89 <das89@tpu.ru>
2024-01-16 21:59:19 +0700

    restructured project files

---
+++ code/market/routes.py
@@ -0,0 +1,20 @@
+from flask import render_template
+from market import app
+from market.models import Item
+
+
+@app.route('/')
+@app.route('/home')
+def home_page():
+    return render_template('home.html')
+
+
+@app.get('/market')
+def market_page():
+    items = Item.query.all()
+    return render_template('market.html', items=items)
+
+
+@app.route('/about/<username>')
+def about(username):
+    return f"<h1>This is about page of {username}</h1>"
