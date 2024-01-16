74f56b7b0c632527b3314fc7b733aa5423968b55
das89 <das89@tpu.ru>
2024-01-17 00:58:11 +0700

    deleted python cached data

--- code/market/__init__.py
+++ code/market/__init__.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
+app.config['SECRET_KEY'] = '02da70d9d2b921b677027e02'
 db = SQLAlchemy(app)
 
 from market import routes
--- code/market/routes.py
+++ code/market/routes.py
@@ -1,5 +1,7 @@
-from flask import render_template
-from market import app
+from flask import render_template, url_for, redirect
+
+from forms import RegisterForm
+from market import app, db
 from market.models import Item, User
 
 
@@ -18,3 +20,14 @@ def market_page():
 @app.route('/about/<username>')
 def about(username):
     return f"<h1>This is about page of {username}</h1>"
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register_page():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        user_to_create = User(name=form.username.data, email=form.email.data, password_hash=form.password1.data)
+        db.session.add(user_to_create)
+        db.session.commit()
+        return redirect(url_for('market_page'))
+    return render_template('register.html', form=form)
