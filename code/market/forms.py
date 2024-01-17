440728eeb14bedeb02f7423c24585b337b247c1e
das89 <das89@tpu.ru>
2024-01-18 00:57:05 +0700

    added password encryption

---
+++ code/market/forms.py
@@ -0,0 +1,11 @@
+from flask_wtf import FlaskForm
+from wtforms.fields.simple import StringField, PasswordField, SubmitField
+from wtforms.validators import Length, Email, EqualTo, DataRequired
+
+
+class RegisterForm(FlaskForm):
+    username = StringField(label='username', validators=[Length(min=2, max=10), DataRequired()])
+    email = StringField(label='email', validators=[Email()])
+    password1 = PasswordField(label='password1', validators=[Length(min=6)])
+    password2 = PasswordField(label='password2', validators=[EqualTo('password1')])
+    submit = SubmitField(label='submit')
